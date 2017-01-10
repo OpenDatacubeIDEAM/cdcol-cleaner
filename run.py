8fae2144f5b5126eefd91f5be7f44f271e5de099
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2017-01-10 12:00:04 -0500

    Creada estructura de archivos inicial

---
+++ run.py
@@ -0,0 +1,8 @@
+# CDCOL Cleaner
+
+from ConfigParser import ConfigParser
+from entities.LockFile import LockFile
+from entities.Connection import Connection
+from exceptions import Exception
+
+import os, sys, datetime
