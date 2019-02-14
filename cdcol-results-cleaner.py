8e498be904c50bae983e8a1e14f1a55b9bd7ce62
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2019-02-14 13:56:03 -0500

    implementing logging

--- cdcol-results-cleaner.py
+++ cdcol-results-cleaner.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
+"""
+SIMPLE LOCKFILE TO DETECT PREVIOUS INSTANCES OF APP
+"""
 
 import os
 import datetime
 import psycopg2
-import psycopg2.extras
+import logging
+
+
+logging.basicConfig(
+    format='%(levelname)s : %(asctime)s : %(message)s',
+    level=logging.DEBUG
+)
+
+# To print loggin information in the console
+logging.getLogger().addHandler(logging.StreamHandler())
+
 
 """
 The default dags results path 
@@ -11,20 +24,21 @@ The default dags results path
 DAGS_RESULTS_PATH = '/web_storage/results'
 
 
-def get_dags_from_db(**kwargs):
+def get_old_dags_from_db(**kwargs):
     """
-        Return the dag ids whose execution date exceeds 
-        a certain number of days.
+    
+    Return the dag ids whose execution date exceeds 
+    a certain number of days.
 
-        Args:
-            kwargs (dict): Contains connection data 
-                such host, dbname, user, passwd and days.
-                If the execution date of the dag exceeds 
-                a certain number of days it will be considered 
-                in the result list.
+    Args:
+        kwargs (dict): Contains connection data 
+            such host, dbname, user, passwd and days.
+            If the execution date of the dag exceeds 
+            a certain number of days it will be considered 
+            in the result list.
 
-        Returns:
-            list: of dags executed certain number of day ago.
+    Returns:
+        list: of dags executed certain number of day ago.
     """
     query_format = (
         'SELECT '
@@ -67,10 +81,22 @@ def get_dags_from_db(**kwargs):
     return rows
 
 
-def update_dag_exection_db(**kwargs):
+def mark_dag_results_as_deleted_db(**kwargs):
+    """ Mark result forder as deleted in the db.
 
+    Mark a result as deleted in 'execution_execution' table 
+    created by the web componnent. This table keeps track of the results 
+    generate by airflow diring an execution.
+
+    Args:
+        kwargs (dict): Must contain the dag_id to be 
+            updated.
+    """
+
+    # The date on which the dag was deleted
     kwargs['results_deleted_at'] = datetime.datetime.now()
 
+    # We mark in the db a result as deleted
     query_format = (
         'UPDATE execution_execution SET '
         'results_available=FALSE, '
@@ -100,26 +126,35 @@ def update_dag_exection_db(**kwargs):
     connection.close()
 
 
-def get_dags_older_result_paths(**kwargs):
+def move_dag_to_history_folder(dag_path):
+    """Move a dag to a history directory
 
-    rows = get_dags_older_result_paths(**kwargs)
+    Args:
+        dag_path (str): path to the dag file
+    """
+    dag_history_path = os.path.join(DAGS_HISTORY_PATH,dag_id)
+    os.rename(dag_path, dag_history_path)
+
+
+def delete_dag_results_folders(**kwargs):
+    """Delete dag result folder in web_storage.
+
+    Args:
+        kwargs (dict): Contains db contecion data such us
+            dbname, user, passwd, host.
+    """
+
+    rows = get_old_dags_from_db(**kwargs)
 
     # Collect the complete path of the 
     # dags that will be removomed
-    dags_paths = []
     for row in rows:
         dag_id = row[1]
-        dag_path = os.path.join(DAGS_RESULTS_PATH,dag_id)
-        dags_paths.append((dag_id,dag_path))
-
-    return dags_paths
-
-
-def delete_dag_results_folders(dags_paths,**kwargs):
-    for dag_id, dag_path in dags_paths:
+        dag_path = row[10]
         if os.path.exists(dag_path):
             os.remove(dag_path)
             update_dag_exection_db(**kwargs)
+            move_dag_to_history_folder(dags_path)        
 
 
 if __name__ == '__main__':
@@ -132,4 +167,4 @@ if __name__ == '__main__':
         'days': 0,
     }
 
-    dags_paths = get_dags_older_result_paths(**data)
+    dags_paths = get_old_dags_result_paths(**data)
